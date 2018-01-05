8ef4775173042d71170429d03f7a7176cb29607a
miadevelop <33178196+miadevelop@users.noreply.github.com>
2018-01-05 13:07:06 +0100

    Test1

---
+++ HelloForGit/HelloForGit/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloForGit
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string part1 = " This is an exercise project for GitHub written in C#.";
+            string part2 = "The author is Michael Abendroth.";
+            Console.WriteLine("{0} {1}", part1, part2);
+            Console.ReadLine();
+        }
+    }
+}
